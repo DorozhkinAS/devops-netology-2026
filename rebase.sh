aa860688aa788f4f328699fc9f3752b821829da4
Artem <vechniy.vrag@gmail.com>
2026-05-13 00:03:57 +0300

    xbranching/.rebase.sh

--- rebase.sh
+++ rebase.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+!/bin/bash
 # display command line options
-
 count=1
 for param in "$@"; do
+<<<<<<< HEAD
     echo "\$@ Parameter #$count = $param"
+=======
+>>>>>>> dc4688f... git 2.3 rebase @ instead *
     count=$(( $count + 1 ))
 done
-
-echo "====="
