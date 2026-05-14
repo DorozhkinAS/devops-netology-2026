71ad156976e785f2431f568b881cd17428e1ea3e
Artem <vechniy.vrag@gmail.com>
2026-05-14 21:10:12 +0300

    Merge branch 'git-merge'

--- rebase.sh
+++ rebase.sh
@@ -2,14 +2,8 @@
 # display command line options
 count=1
 for param in "$@"; do
-<<<<<<< HEAD
 
     echo "\$@ Parameter #$count = $param"
-
     echo "Next parameter: $param"
->>>>>>> f4ac62c (git-rebase 2)
-=======
-    echo "Next parameter: $param"
->>>>>>> refs/remotes/origin/git-rebase
     count=$(( $count + 1 ))
 done
